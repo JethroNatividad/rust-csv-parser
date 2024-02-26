62c70d0a8238940632fdec7b009e5ebe99a4af99
JethroNatividad <natividad.jet@gmail.com>
2024-02-26 10:09:14 +0800

    write problem

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,14 @@
 // Create a program that reads a csv and outputs a table
+/// Inputs: csv file with:
+// Ling,Mai,55900
+// Johnson,Jim,56500
+// Jones,Aaron,46000
+// Jones,Chris,34500
+// Swift,Geoffrey,14200
+// Xiong,Fong,65000
+// Zarnecki,Sabrina,51500
+// Process: make this into a vector of structs
+// Outputs: table
 fn main() {
     println!("Hello, world!");
 }
